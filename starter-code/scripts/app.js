24f750650aacd7636fdeaee325fd01cf871db9d4
cfcrawford <camilafcrawford@gmail.com>
2016-07-05 22:38:04 -0700

    creates initial files

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,1 +1,11 @@
 angular.module('CardsAgainstAssembly', []);
+
+function wdiCard(){
+  var directive = {
+    restrict: 'E',
+    replace: true,
+    templateUrl: "templates/cardDirective.html"
+  };
+
+  return directive;
+}
